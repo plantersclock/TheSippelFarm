ec4c47451e3d1625ef41c455a3b3d05371edf9ab
Wright <wright-matthew2@aramark.com>
2019-10-25 11:21:06 -0400

    test 5

--- events/forms.py
+++ events/forms.py
@@ -25,6 +25,6 @@ class AttendeeScheduleForm(forms.ModelForm):
     class Meta:
         model = ScheduledAttendee
         fields = ["attendee", "day", "time", "duration"]
-        widgets = { "day": DatePickerInput(attrs={"onfocus":"blur()"}) ,
+        widgets = { "day": DatePickerInput(attrs={"focus":"blur()"}) ,
                     "time": TimePickerInput(attrs={"InputType":"TYPE_NULL"})
                 }
